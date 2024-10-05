47950fda3e08de0b501c36af77b66652749048d7
eyovvalupe <rylantwcn705172@hotmail.com>
2024-10-05 10:23:25 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider, THEME } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://github.com/KalidSittner/blockchain_network/blob/main/dapp_manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider
@@ -18,9 +18,18 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           imageUrl: "https://tonkeeper.com/assets/tonconnect-icon.png",
           aboutUrl: "https://tonkeeper.com",
           universalLink: "https://app.tonkeeper.com/ton-connect",
-          bridgeUrl: "http://localhost:8080/bridge",
+          bridgeUrl: "http://localhost:80/bridge/tonkeeper",
           platforms: ["ios", "android", "macos", "windows", "linux"]
-        },     
+        },
+        {
+          appName: "mytonwallet",
+          name: "MyTonWallet",
+          imageUrl: "https://static.mytonwallet.io/icon-256.png",
+          aboutUrl: "https://mytonwallet.io",
+          universalLink: "https://connect.mytonwallet.org",
+          bridgeUrl: "http://localhost:80/bridge/mytonwallet",
+          platforms: ["ios", "android", "macos", "windows", "linux"]
+        },
       ]
     }}
   >
